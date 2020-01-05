7e4c1e156e12a3d1ec9af782b1876f7567bb11f5
Jayanti2906 <jayantisingh2906@gmail.com>
2020-01-05 15:08:39 +0530

    added lesson view and video feature

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from memberships.models import Membership
 
+from django.urls import reverse
+
 class Course(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=120)
@@ -9,6 +11,15 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('courses:detail',kwargs ={'slug':self.slug})
+    #for making it a property we apply @property
+    @property
+    def lessons(self):
+        return self.lesson_set.all().order_by('position')
+
+
 
 class Lesson(models.Model):
     slug = models.SlugField()
@@ -20,3 +31,9 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('courses:lesson-detail',kwargs ={
+            'course_slug':self.course.slug,
+            'lesson_slug':self.slug,
+            })
